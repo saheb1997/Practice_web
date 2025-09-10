a17a41da2e790ff52b4543b0177a969bd6aa5401
saheb_sarkar <sahebs450@gmail.com>
2025-09-10 19:23:17 +0530

    first 5 Dom callenge complete

--- Javascript/Dom_manupulation/script1.js
+++ Javascript/Dom_manupulation/script1.js
@@ -20,6 +20,35 @@ document
          
          
     });
+
+// example-3 Manipulation Dom elements
+document.getElementById("changeorder").addEventListener('click',
+    function(){
+        let coffeType = document.getElementById("coffeType");
+        coffeType.textContent = "Espresso";
+        coffeType.style.background = "brown";
+        coffeType.style.padding = "5px";
+    }
+)
+
+//Example 4: create and Inserting Elements 
+document.getElementById("addNewItem").addEventListener('click',
+    function(){
+        let newItem = document.createElement('li');
+        newItem.textContent = "Eggs";
+
+        document.getElementById("shoppingList").appendChild(newItem);
+    }
+)
+
+// Example 5 : Removing the Dom elements 
+document.getElementById("removeLastTask").addEventListener('click',
+    function(){
+        let taskList = document.getElementById("taskList");
+        taskList.lastElementChild.remove()
+    }
+)
+
  
 
  
