aa6c8ced68b4ebf364eb545936d2c36a0d4b9c41
saheb_sarkar <sahebs450@gmail.com>
2026-02-20 00:34:28 +0530

    color change project is added

--- Javascript/03_basic/01_fucntions.js
+++ Javascript/03_basic/01_fucntions.js
@@ -1,40 +1,46 @@
 function addtwonumber(number1, number2) {
   //here we give parameters
-  let num1 = Number(number1)
-  let num2 = Number(number2)
-  console.log(num1 + num2)
-  return num1 + num2
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  console.log(num1 + num2);
+  return num1 + num2;
 }
 
 // const val = addtwonumber(3, 4) //here it called arguments
 // console.log(val)
 
-function loginUserMessage(username = 'sam') {
+function loginUserMessage(username = "sam") {
   if (!username) {
-    console.log('pls enter name')
+    // console.log('pls enter name')
   }
 
-  return ` ${username} just loged in`
+  return ` ${username} just loged in`;
 }
 
 // console.log(loginUserMessage())
 
 // when we don't know the how many parameter will come
 function calculateCartPrice(...num1) {
-  return num1
+  let val = 0;
+  for (let index = 0; index < num1.length; index++) {
+    val += num1[index];
+  }
+  console.log(val);
+
+  return num1;
 }
 
-// console.log(calculateCartPrice(100, 200, 300))
+console.log(calculateCartPrice(100, 200, 300));
 
 function calculateCartPrice1(num1, num2, ...num3) {
   //   console.log(num1, num2, num3)
-  return 0
+  return 0;
 }
 
-calculateCartPrice1(100, 200, 300, 400, 500)
+calculateCartPrice1(100, 200, 300, 400, 500);
 
-const myNewArray = [200, 400, 100, 600]
+const myNewArray = [200, 400, 100, 600];
 function retunrSecondvalue(getArray) {
-  return getArray[1]
+  return getArray[1];
 }
-console.log(retunrSecondvalue(myNewArray))
+console.log(retunrSecondvalue(myNewArray));
